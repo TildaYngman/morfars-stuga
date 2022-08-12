da7f1e11e7585cb7d1f957919de816d35e3dc50b
felix-odenhall <felix.odenhall@gmail.com>
2022-08-12 12:51:15 +0200

    updated layout for Q&A component

--- client/components/questionsAndAnswersComponent.js
+++ client/components/questionsAndAnswersComponent.js
@@ -1,11 +1,10 @@
 export default function QuestionsAndAnswersComponent({ temp }) {
   return (
-    <>
-      <p>Question</p>
-      <p>{temp.question}</p>
-      <p>Answer</p>
-      <p>{temp.answer}</p>
-      <br></br>
-    </>
+    <section className="mt-4 p-2 bg-slate-200 flex flex-col">
+      <p className=" text-lg font-medium">Question</p>
+      <p className=" mb-4">{temp.question}</p>
+      <p className=" text-lg font-medium">Answer</p>
+      <p className=" mb-2">{temp.answer}</p>
+    </section>
   );
 }

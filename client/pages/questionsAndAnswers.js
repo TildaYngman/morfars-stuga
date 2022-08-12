b95ec97a15e70ba9db551e88eff5c7157ed7fdf7
felix-odenhall <felix.odenhall@gmail.com>
2022-08-12 11:23:03 +0200

    mapping through temporary JSON placeholder, to get information from an external API

--- client/pages/questionsAndAnswers.js
+++ client/pages/questionsAndAnswers.js
@@ -1,9 +1,16 @@
 import QuestionsAndAnswersComponent from "../components/questionsAndAnswersComponent";
 
-export default function QuestionsAndAnswers() {
+export default function QuestionsAndAnswers({ users }) {
   return (
     <>
-      <h1>Questions and Answers</h1>;
+      <h1>Questions and Answers</h1>
+      {users.map((temp) => {
+        return (
+          <div key={temp.id}>
+            <QuestionsAndAnswersComponent temp={temp} />
+          </div>
+        );
+      })}
     </>
   );
 }

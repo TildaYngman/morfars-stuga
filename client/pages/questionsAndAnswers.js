83d89993322c34162833d0824eb1ea3617d07b62
felix-odenhall <felix.odenhall@gmail.com>
2022-08-12 11:15:15 +0200

    added getStaticProps with temporary JSON placeholder

--- client/pages/questionsAndAnswers.js
+++ client/pages/questionsAndAnswers.js
@@ -1,3 +1,14 @@
 export default function QuestionsAndAnswers() {
   return <h1>Questions and Answers</h1>;
 }
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+}

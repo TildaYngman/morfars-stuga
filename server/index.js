96662613e664529287ac9fac0597255c16b3e028
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-12 09:06:43 +0200

    Added mongoclient, URL  and db in server index file

--- server/index.js
+++ server/index.js
@@ -3,3 +3,12 @@ import express from "express";
 import mongodb from "mongodb";
 import cors from "cors";
 
+//Configure MongoDB
+const MONGODB_URL =
+  process.env.MONGODB_URL ||
+  "mongodb+srv://admin:eoiAjqS8s5JGlTj9@cluster0.qyz0gie.mongodb.net/test";
+
+const mongoClient = new mongodb.MongoClient(MONGODB_URL);
+mongoClient.connect();
+
+const db = mongoClient.db("cabin");

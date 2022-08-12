fd61d4f9e1c95f3bae07b2bb88a2d3ce73528cf0
felix-odenhall <felix.odenhall@gmail.com>
2022-08-12 13:13:34 +0200

    updated screen-size in tailwind.config, added different colors, based on screen-size, moved Link from bottom to top

--- client/components/questionsAndAnswersComponent.js
+++ client/components/questionsAndAnswersComponent.js
@@ -1,6 +1,6 @@
 export default function QuestionsAndAnswersComponent({ temp }) {
   return (
-    <section className="mt-4 p-2 bg-slate-200 flex flex-col">
+    <section className="mt-2 p-2 rounded-md bg-slate-200 sm:bg-red-200 md:bg-green-200 lg:bg-blue-100 xl:bg-orange-200 flex flex-col max-w-md">
       <p className=" text-lg font-medium">Question</p>
       <p className=" mb-4">{temp.question}</p>
       <p className=" text-lg font-medium">Answer</p>

896a9e45d93457d38aceec4396e0d9ac769ad5d5
felix-odenhall <felix.odenhall@gmail.com>
2022-08-12 11:27:25 +0200

    added link to questions and answers page on the home page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline text-cyan-300">Project</h1>
+    <>
+      <h1 className="text-3xl font-bold underline text-cyan-300">Project</h1>
+      <Link href="/questionsAndAnswers">Questions and Answers</Link>
+    </>
   );
 }

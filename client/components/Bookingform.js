11f7a87283bd15417447b578d12e2e14fd39412d
felix-odenhall <felix.odenhall@gmail.com>
2022-08-15 10:34:36 +0200

    added basic style to the form

--- client/components/Bookingform.js
+++ client/components/Bookingform.js
@@ -1,15 +1,43 @@
 export default function BookingForm() {
   return (
     <>
-      <form action="/send-data-here" method="post">
-        <label for="title">Title:</label>
-        <input type="text" id="title" name="title" />
-        <label for="requestDate">When would you like to book:</label>
-        <input type="date" id="requestDate" name="requestDate"></input>
-        <label for="message">Message:</label>
-        <input type="text" id="message" name="message" />
-        <label for="email">Your Email:</label>
-        <input type="text" id="email" name="email" />
+      <form className=" flex flex-col" action="/send-data-here" method="post">
+        <label className=" mb-2" for="title">
+          Title:
+        </label>
+        <input
+          className=" border-solid border-2 border-slate-500"
+          type="text"
+          id="title"
+          name="title"
+        />
+        <label className=" mb-2" for="requestDate">
+          When would you like to book:
+        </label>
+        <input
+          className=" border-solid border-2 border-slate-500"
+          type="date"
+          id="requestDate"
+          name="requestDate"
+        ></input>
+        <label className=" mb-2" for="message">
+          Message:
+        </label>
+        <input
+          className=" border-solid border-2 border-slate-500"
+          type="text"
+          id="message"
+          name="message"
+        />
+        <label className=" mb-2" for="email">
+          Your Email:
+        </label>
+        <input
+          className=" border-solid border-2 border-slate-500"
+          type="text"
+          id="email"
+          name="email"
+        />
         <button type="submit">Submit</button>
       </form>
     </>

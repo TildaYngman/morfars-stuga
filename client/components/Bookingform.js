e641973729aee2233262be35bb967aea6bf3db7f
felix-odenhall <felix.odenhall@gmail.com>
2022-08-15 10:19:32 +0200

    created bookingRequest.js to pages, created BookingForm.js to components

---
+++ client/components/Bookingform.js
@@ -0,0 +1,3 @@
+export default function BookingForm({ temp }) {
+  return <></>;
+}
---
+++ client/pages/bookingRequest.js
@@ -0,0 +1,7 @@
+export default function BookingRequest({ temp }) {
+  return (
+    <>
+      <h1>Booking Request</h1>
+    </>
+  );
+}

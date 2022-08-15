ad3514beda45b6088a6cdf6f7391daadbef615fa
felix-odenhall <felix.odenhall@gmail.com>
2022-08-15 10:27:41 +0200

    created a form in BookingForm.js, imported it to BookingRequest.js page

--- client/components/Bookingform.js
+++ client/components/Bookingform.js
@@ -1,3 +1,17 @@
-export default function BookingForm({ temp }) {
-  return <></>;
+export default function BookingForm() {
+  return (
+    <>
+      <form action="/send-data-here" method="post">
+        <label for="title">Title:</label>
+        <input type="text" id="title" name="title" />
+        <label for="requestDate">When would you like to book:</label>
+        <input type="date" id="requestDate" name="requestDate"></input>
+        <label for="message">Message:</label>
+        <input type="text" id="message" name="message" />
+        <label for="email">Your Email:</label>
+        <input type="text" id="email" name="email" />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
--- client/pages/bookingRequest.js
+++ client/pages/bookingRequest.js
@@ -1,7 +1,10 @@
+import BookingForm from "../components/Bookingform";
+
 export default function BookingRequest({ temp }) {
   return (
     <>
       <h1>Booking Request</h1>
+      <BookingForm />
     </>
   );
 }

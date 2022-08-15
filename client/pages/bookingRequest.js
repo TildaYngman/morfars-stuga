ae839abb7e0bea36558bbd21c57ab454c660d0b9
felix-odenhall <felix.odenhall@gmail.com>
2022-08-15 10:34:36 +0200

    added basic style to the form

--- client/pages/bookingRequest.js
+++ client/pages/bookingRequest.js
@@ -1,6 +1,6 @@
 import BookingForm from "../components/Bookingform";
 
-export default function BookingRequest({ temp }) {
+export default function BookingRequest() {
   return (
     <>
       <h1>Booking Request</h1>

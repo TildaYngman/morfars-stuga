c071109c1724dedcd86be41e5da6d023cdcbd9ac
felix-odenhall <felix.odenhall@gmail.com>
2022-08-15 10:49:47 +0200

    created handleSubmit function for the form, updated information in the form

--- client/components/Bookingform.js
+++ client/components/Bookingform.js
@@ -1,17 +1,50 @@
 export default function BookingForm() {
+  const handleSubmit = async (event) => {
+    // Stop the form from submitting and refreshing the page.
+    event.preventDefault();
+
+    // Get data from the form.
+    const data = {
+      firstName: event.target.firstName.value,
+      lastName: event.target.lastName.value,
+      requestDate: event.target.requestDate.value,
+      message: event.target.message.value,
+      email: event.target.email.value,
+    };
+
+    console.log(data);
+    alert(
+      `Thank you ${data.firstName} ${data.lastName}, your booking request has been successfully submited.`
+    );
+  };
+
   return (
     <>
-      <form className=" flex flex-col" action="/send-data-here" method="post">
-        <label className=" mb-2" for="title">
-          Title:
+      <form
+        className=" flex flex-col"
+        action="/send-data-here"
+        method="post"
+        onSubmit={handleSubmit}
+      >
+        <label className=" mb-2" htmlFor="firstName">
+          First name:
         </label>
         <input
           className=" border-solid border-2 border-slate-500"
           type="text"
-          id="title"
-          name="title"
+          id="firstName"
+          name="firstName"
         />
-        <label className=" mb-2" for="requestDate">
+        <label className=" mb-2" htmlFor="lastName">
+          Last name:
+        </label>
+        <input
+          className=" border-solid border-2 border-slate-500"
+          type="text"
+          id="lastName"
+          name="lastName"
+        />
+        <label className=" mb-2" htmlFor="requestDate">
           When would you like to book:
         </label>
         <input
@@ -20,7 +53,7 @@ export default function BookingForm() {
           id="requestDate"
           name="requestDate"
         ></input>
-        <label className=" mb-2" for="message">
+        <label className=" mb-2" htmlFor="message">
           Message:
         </label>
         <input
@@ -29,7 +62,7 @@ export default function BookingForm() {
           id="message"
           name="message"
         />
-        <label className=" mb-2" for="email">
+        <label className=" mb-2" htmlFor="email">
           Your Email:
         </label>
         <input

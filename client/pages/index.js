5f4026d1b6dc8bcc484072a5f28dc39509fa1e56
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-16 14:54:30 +0200

    Added a index file in components where we export all components

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import Greeting from "../components/Greeting";
+import { Greeting, MessageForm } from "../components/index";
 
 export default function Home() {
   return (
@@ -10,6 +10,7 @@ export default function Home() {
       <Greeting />
       <Link href="/questionsAndAnswers">Questions and Answers</Link>
       <Link href="/about">About The Cabin</Link>
+      <MessageForm />
     </>
   );
 }

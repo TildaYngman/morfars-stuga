b87317e60d2c677f7f7f42b07480ed70d9354068
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-16 14:54:30 +0200

    Added a index file in components where we export all components

--- client/components/MessageForm.js
+++ client/components/MessageForm.js
@@ -1,4 +1,13 @@
 export default function Form() {
-    return (
-    )
-  }
+  return (
+    <form action="/api/form" method="post">
+      <label htmlFor="first">First Name</label>
+      <input type="text" id="first" name="first" required />
+
+      <label htmlFor="last">Last Name</label>
+      <input type="text" id="last" name="last" required />
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
---
+++ client/components/index.js
@@ -0,0 +1,4 @@
+export { default as Bookingform } from "./Bookingform";
+export { default as Greeting } from "./Greeting";
+export { default as MessageForm } from "./MessageForm";
+export { default as questionsAndAnswersComponent } from "./questionsAndAnswersComponent";

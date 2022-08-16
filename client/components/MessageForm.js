b13b02641c866f3d927f22f2a8d6a3ee8f4a8773
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-16 15:05:25 +0200

    Simple message form added in MessageForm

--- client/components/MessageForm.js
+++ client/components/MessageForm.js
@@ -1,13 +1,17 @@
 export default function Form() {
   return (
-    <form action="/api/form" method="post">
-      <label htmlFor="first">First Name</label>
-      <input type="text" id="first" name="first" required />
-
-      <label htmlFor="last">Last Name</label>
-      <input type="text" id="last" name="last" required />
-
-      <button type="submit">Submit</button>
-    </form>
+    <div className="mt-2 p-2 rounded-md bg-slate-200 sm:bg-red-200 md:bg-green-200 lg:bg-blue-100 xl:bg-orange-200  flex justify-center flex-col">
+      <form>
+        <label htmlFor="email">Email</label>
+        <br />
+        <input type="email" id="first" name="email" required />
+        <br />
+        <label htmlFor="message">Message</label>
+        <br />
+        <input type="text" id="message" name="message" required />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   );
 }

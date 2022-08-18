97ba77455bf1bf46cb8d85d533e9fe0ba5989236
felix-odenhall <felix.odenhall@gmail.com>
2022-08-18 13:19:03 +0200

    removded unused code and comments

--- client/pages/questionsAndAnswers.js
+++ client/pages/questionsAndAnswers.js
@@ -11,11 +11,6 @@ export default function QuestionsAndAnswers({ items }) {
           Questions and Answers
         </h1>
         <QueststionCategory temp={items} />
-        {/* <section className="flex flex-col items-start">
-          {items.map((temp) => {
-            return <QuestionsAndAnswersComponent key={temp._id} temp={temp} />;
-          })}
-        </section> */}
       </main>
     </>
   );

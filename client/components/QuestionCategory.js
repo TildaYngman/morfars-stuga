8e6f10ca4b34e4bb1bac630a33aeeee92d221c85
felix-odenhall <felix.odenhall@gmail.com>
2022-08-18 11:48:55 +0200

    updated handleClick function to log questions from the correct categories

--- client/components/QuestionCategory.js
+++ client/components/QuestionCategory.js
@@ -1,4 +1,8 @@
+import { useState, useEffect } from "react";
+
 export default function QuestionsAndAnswersComponent({ temp }) {
+  const [questionArr, setQuestionArr] = useState([]);
+
   const categoryArray = [];
   const firstObjectOfCategories = [];
 
@@ -8,14 +12,32 @@ export default function QuestionsAndAnswersComponent({ temp }) {
       firstObjectOfCategories.push(object);
     }
   });
-  console.log(categoryArray);
-  console.log(temp);
+
+  // function handleClick(e) {
+  //   temp.forEach((element) => {
+  //     if (e.target.id === element.category) {
+  //       console.log(element.question);
+  //       setQuestionArr(questionArr.push(element));
+  //     }
+  //     console.log("Question ARR: ", questionArr);
+  //   });
+  //
 
   function handleClick(e) {
+    const newArray = [];
     temp.forEach((element) => {
-      if (e.target.id === element.category) console.log(element.question);
+      if (e.target.id === element.category) {
+        newArray.push(element); //add element to new array
+        setQuestionArr(questionArr); //overwrite state with new array with element pushed in
+        console.log(element.question);
+        console.log(newArray);
+      }
     });
   }
+
+  // useEffect(() => {
+  //   console.log("Question ARR: ", questionArr);
+  // }, [questionArr]);
 
   return (
     <>

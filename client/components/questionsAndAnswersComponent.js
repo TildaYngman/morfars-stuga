0861981a134144349e6b683395c28cd8ad035a32
felix-odenhall <felix.odenhall@gmail.com>
2022-08-18 13:11:24 +0200

    created a function that shows correct questions when clicking a category, moved using the using the questionAndAnswerComponent to render the questions and answers

--- client/components/questionsAndAnswersComponent.js
+++ client/components/questionsAndAnswersComponent.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { AiOutlineDown } from "react-icons/ai";
 
-export default function QuestionsAndAnswersComponent({ temp }) {
+export default function QuestionsAndAnswersComponent({ item }) {
   const [isShown, setIsShown] = useState(false);
 
   const handleClick = (event) => {
@@ -18,12 +18,12 @@ export default function QuestionsAndAnswersComponent({ temp }) {
         className=" p-2 mb-3 w-full bg-slate-300 flex items-center justify-between"
         onClick={handleClick}
       >
-        <p className="text-left">{temp.question}</p>
+        <p className="text-left">{item.question}</p>
         <AiOutlineDown className="ml-2" />
       </button>
       {isShown && (
         <div className="mb-3">
-          <p className="text-left">{temp.answer}</p>
+          <p className="text-left">{item.answer}</p>
         </div>
       )}
     </>

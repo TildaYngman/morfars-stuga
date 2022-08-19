27e4bce5524424c75b0ba7a7e0b42c0cbb3ff4ae
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-19 14:23:50 +0200

    Removed guest phone

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,7 +9,6 @@ export default function Home() {
   const [guestName, setGuestName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const [guestPhone, setGuestPhone] = useState("");
   const [title, setTitle] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
@@ -26,14 +25,13 @@ export default function Home() {
         setGuestName={setGuestName}
         setEmail={setEmail}
         setMessage={setMessage}
-        setGuestPhone={setGuestPhone}
         setTitle={setTitle}
         setSubmitted={setSubmitted}
         guestName={guestName}
         email={email}
         message={message}
-        guestPhone={guestPhone}
         title={title}
+        submitted={submitted}
       />
     </>
   );

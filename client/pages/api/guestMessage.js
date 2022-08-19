2fded1c39bfe20037f3a83e4d1ef6af1110fb8f8
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-19 14:08:35 +0200

    Added a const in guestMessage for the email to require sendgrid and that the sendgrid need the env variable

--- client/pages/api/guestMessage.js
+++ client/pages/api/guestMessage.js
@@ -1,5 +1,6 @@
 //Any file inside of the pages/api folder is treated as an API endpoint.
-
+const email = require('@sendgrid/mail');
+email.setApiKey(process.env.SENDGRID_API_KEY);
 
 export default function (req, res) {
   let nodemailer = require("nodemailer");

e454b4fbaf62b13f1feba7ba4dc3297984dd2d89
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-19 14:02:40 +0200

    Added a new file in the root of the project for the env variable

--- client/pages/api/guestMessage.js
+++ client/pages/api/guestMessage.js
@@ -1,5 +1,8 @@
 //Any file inside of the pages/api folder is treated as an API endpoint.
 
+
 export default function (req, res) {
+  let nodemailer = require("nodemailer");
+  const transporter = nodemailer.createTransport({});
   console.log(req.body);
 }

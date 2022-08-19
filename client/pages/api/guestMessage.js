d79c0553b7eaa4ba677f33ac49af08f4ae8b873f
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-19 14:21:19 +0200

    connection to sendgrid set up but not working server error 500

--- client/pages/api/guestMessage.js
+++ client/pages/api/guestMessage.js
@@ -1,9 +1,22 @@
 //Any file inside of the pages/api folder is treated as an API endpoint.
-const email = require('@sendgrid/mail');
+const email = require("@sendgrid/mail");
 email.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default function (req, res) {
-  let nodemailer = require("nodemailer");
-  const transporter = nodemailer.createTransport({});
-  console.log(req.body);
-}
+export default async (req, res) => {
+  const body = JSON.parse(req.body);
+
+  const message = `
+  Name: ${body.guestName}\r\n
+  Email: ${body.email}\r\n
+  Message: ${body.message}
+`;
+  const data = {
+    to: "tildayngman@gmail.com",
+    from: "matilda.yngman@hyperisland.se",
+    subject: `New message from ${body.guestName}`,
+    text: message,
+    html: message.replace(/\r\n/g, "<br />"),
+  };
+  await email.send(data);
+  res.status(200).json({ status: "OK" });
+};

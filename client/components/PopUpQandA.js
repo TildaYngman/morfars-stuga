19080a20bce596de672b9ca0b9d044198c520e4b
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-22 12:01:58 +0200

    Set up a useeState which originally is false changing it to tru when clicked and logging to see the change

--- client/components/PopUpQandA.js
+++ client/components/PopUpQandA.js
@@ -1,8 +1,13 @@
+import { useState } from "react";
+
 export default function PopUpQandA() {
+  const [displayContent, setDisplayContent] = useState(false);
   function handleClick() {
     console.log("im a button");
+    setDisplayContent(true);
+    console.log(displayContent);
   }
-
+  console.log(displayContent);
   return (
     <>
       <button onClick={handleClick}>PopUpQandA</button>
@@ -10,10 +15,11 @@ export default function PopUpQandA() {
   );
 }
 
-//Add a Button
+//Add a Button X
+//Add a handleClick function to the button. X
+//handleClick - UseState(false) set to true when clicked open and false when closed.
 //Clicking the button should open a container.
 //handleClick - opens container, toggles open/close to show content inside the container.
-//handleClick - UseState(false) set to true when clicked open and false when closed.
 //handleClick - function updates the useState and updates button icon when toggled.
 
 //Container - importing QuestionCategory.js component

1ca371e85dcb58727e789510c63158f4daa353b2
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-22 11:52:59 +0200

    Added a handleClick function to the button handleClick is logging when the button is clicked

--- client/components/PopUpQandA.js
+++ client/components/PopUpQandA.js
@@ -1,12 +1,16 @@
 export default function PopUpQandA() {
+  function handleClick() {
+    console.log("im a button");
+  }
+
   return (
     <>
-    <button></button>
+      <button onClick={handleClick}>PopUpQandA</button>
     </>
   );
 }
 
-//Add a Button 
+//Add a Button
 //Clicking the button should open a container.
 //handleClick - opens container, toggles open/close to show content inside the container.
 //handleClick - UseState(false) set to true when clicked open and false when closed.

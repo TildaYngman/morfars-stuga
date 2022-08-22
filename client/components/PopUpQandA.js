d8e6eba1e3ff3a4280b11e8a49a6b114dcdfb8ad
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-22 11:38:37 +0200

    Psuedo coding button and handleClick and added a button in return

--- client/components/PopUpQandA.js
+++ client/components/PopUpQandA.js
@@ -1,6 +1,17 @@
 export default function PopUpQandA() {
   return (
     <>
+    <button></button>
     </>
   );
 }
+
+//Add a Button 
+//Clicking the button should open a container.
+//handleClick - opens container, toggles open/close to show content inside the container.
+//handleClick - UseState(false) set to true when clicked open and false when closed.
+//handleClick - function updates the useState and updates button icon when toggled.
+
+//Container - importing QuestionCategory.js component
+//Container - full overlay when mobile, small overlay when bigger screen.
+//Container - Add button inside container to close container

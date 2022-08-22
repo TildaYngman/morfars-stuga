2e4c36b4e49784e7c008124ee343e10f537fde67
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-22 11:17:40 +0200

    Added basic function structure and exporting component in components/index.js

--- client/components/index.js
+++ client/components/index.js
@@ -2,3 +2,4 @@ export { default as Bookingform } from "./Bookingform";
 export { default as Greeting } from "./Greeting";
 export { default as MessageForm } from "./MessageForm";
 export { default as questionsAndAnswersComponent } from "./questionsAndAnswersComponent";
+export { default as PopUpQandA } from "./PopUpQandA";

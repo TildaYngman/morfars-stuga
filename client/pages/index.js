7434f89792ae8a0c81c25970385dad077d3e4529
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-22 11:44:52 +0200

    Imported popupQandA to index.js and installed react-icons

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import { Greeting, MessageForm } from "../components/index";
+import { Greeting, MessageForm, PopUpQandA } from "../components/index";
 import { useState } from "react";
 
 export default function Home() {
@@ -19,6 +19,8 @@ export default function Home() {
       <Link href="/about">About The Cabin</Link>
       <br />
       <Link href="/bookingRequest">booking</Link>
+      <br />
+      <PopUpQandA />
       <br />
       <MessageForm
         setGuestName={setGuestName}

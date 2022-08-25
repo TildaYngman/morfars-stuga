85be78db4cecd49427bf110ae636643b25b1cdb8
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-25 15:30:36 +0200

    Test to fetch bookableWeeks collection in the same fetch as QandA

--- client/components/index.js
+++ client/components/index.js
@@ -1,7 +1,7 @@
 export { default as Bookingform } from "./Bookingform";
 export { default as Greeting } from "./Greeting";
 export { default as MessageForm } from "./MessageForm";
-export { default as questionsAndAnswersComponent } from "./questionsAndAnswersComponent";
+export { default as QuestionsAndAnswersComponent } from "./QuestionsAndAnswersComponent";
 export { default as PopUpQandA } from "./PopUpQandA";
 export { default as QuestionCategory } from "./QuestionCategory";
 export { default as AvailableWeeks } from "./AvailableWeeks";

1e3402f8c91a2330091c56b2ed274abce6dd9a16
felix-odenhall <felix.odenhall@gmail.com>
2022-08-25 10:30:22 +0200

    created a function to only push the objects with the category of Bokningsinformation to BookingInformationArr

--- client/components/BookingInformation.js
+++ client/components/BookingInformation.js
@@ -1,7 +1,29 @@
 export default function BookingInformationComponent({ temp }) {
+  console.log(temp);
+
+  const BookingInformationArr = [];
+
+  temp.forEach((item) => {
+    if (item.category === "Bokningsinformation")
+      BookingInformationArr.push(item);
+  });
+
+  console.log(BookingInformationArr);
+
+  //   function filterByCategory(temp) {
+  //     if (temp.category === "Bokningsinformation") {
+  //       return temp;
+  //     }
+  //   }
+
+  //   const newArr = temp.filter(filterByCategory);
+
+  //   console.log(newArr);
+
   return (
     <>
       <h1>Bokningsinformation</h1>
+      <p>{BookingInformationArr[0].question}</p>
     </>
   );
 }

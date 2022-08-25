6a778be54ee45a4752848b3112383010e1b1ae78
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-25 15:30:36 +0200

    Test to fetch bookableWeeks collection in the same fetch as QandA

--- client/pages/bookingRequest.js
+++ client/pages/bookingRequest.js
@@ -15,9 +15,13 @@ export async function getStaticProps() {
   const res = await fetch("http://localhost:28017/QandA");
   const data = await res.json();
 
+  const re = await fetch("http://localhost:28017/bookableWeeks");
+  const weeks = await re.json();
+
   return {
     props: {
       items: data,
+      weeks: weeks,
     },
   };
 }

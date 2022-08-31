4c57bff8bab4d13ee73ac352ba0a5525ce85e5f9
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-08-31 11:19:38 +0200

    Toggle between colors onclick when adding the clicked week to the new state

--- client/pages/bookingRequest.js
+++ client/pages/bookingRequest.js
@@ -20,27 +20,28 @@ export default function BookingRequest({ items, weeks }) {
 
     if (!clickedWeek.includes(week)) {
       clickedWeek.push(week);
+      document
+        .getElementById(`${week._id}`)
+        .classList.add("bg-primary-orange-400");
     } else {
       clickedWeek.splice(clickedWeek.indexOf(week), 1);
+      document
+        .getElementById(`${week._id}`)
+        .classList.remove("bg-primary-orange-400");
     }
 
     setSelectedWeeks([...clickedWeek]);
     console.log(selectedWeeks);
   }
-  // clickedWeek.push(week);
-  //setSelectedWeeks([...clickedWeek]);
-  //console.log(selectedWeeks);
-  // if (clickedWeek.indexOf(week) === -1) {
-  //   clickedWeek.push(week);
-  // }
 
   const rows = weeks.map((week) => {
     if (week.isAvailable === true) {
       return (
         <button
           onClick={() => handleClickedWeek(week)}
-          className=" bg-primary-green-400 text-primary-black text-lg w-72 m-1 p-2 text-left font-semibold"
+          className={`bg-primary-green-400 text-primary-black text-lg w-72 m-1 p-2 text-left font-semibold`}
           key={week._id}
+          id={week._id}
         >
           <p>Vecka</p>
           {week.Vecka}

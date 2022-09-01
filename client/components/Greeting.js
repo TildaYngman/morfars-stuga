c29a863a401786e7c9869f0506c4cb640643b982
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-09-01 14:23:28 +0200

    Changed the Header name of the project and the sizes of the h1 and h2

--- client/components/Greeting.js
+++ client/components/Greeting.js
@@ -13,15 +13,10 @@ export default function Greeting() {
         />
       </div>
       <div className="mt-2 p-2 rounded-md bg-slate-200 sm:bg-red-200 md:bg-green-200 lg:bg-blue-100 xl:bg-orange-200  flex justify-center items-center flex-col">
-        <h1 className="text-2xl font-bold text-center">
-          Syskonen Yngmans Fjällstuga
-        </h1>
-        <h2 className="text-lg font-bold text-center">
+        <h1 className="text-2xl font-semibold">Morfars Stuga</h1>
+        <h2 className="text-xl font-semibold">
           Välkommen till vår fjällstuga i vackra Tänndalen
         </h2>
-        <p className="text-xs">
-          Insert picture of the cabin - scale acording to picture
-        </p>
       </div>
     </>
   );

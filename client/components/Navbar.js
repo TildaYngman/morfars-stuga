1e93ea7aa694b3741d4894218514f818e65dd868
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-09-01 14:57:59 +0200

    changed tag to nav

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,9 +1,9 @@
 export default function Navbar() {
   return (
     <>
-      <Nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+      <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
         <h1>Logo</h1>
-      </Nav>
+      </nav>
     </>
   );
 }

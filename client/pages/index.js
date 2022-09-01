7a0a57897174d709f034dc245daa5c443e85e247
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-09-01 14:57:34 +0200

    Importing and Displaying Navbar in index

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import { Greeting, MessageForm, PopUpQandA } from "../components/index";
+import { Greeting, MessageForm, PopUpQandA, Navbar } from "../components/index";
 import { useState } from "react";
 
 export default function Home({
@@ -20,6 +20,7 @@ export default function Home({
 }) {
   return (
     <>
+      <Navbar />
       <PopUpQandA temp={items} />
       <Greeting />
       <Link href="/questionsAndAnswers">Questions and Answers</Link>

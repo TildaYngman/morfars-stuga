c05a3baf8f81d8466dc66316f26065847a530cce
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-09-01 14:25:04 +0200

    Copied the image and text in the greeting component to the index home page instead Greeting-component will be removed

--- client/pages/index.js
+++ client/pages/index.js
@@ -20,6 +20,21 @@ export default function Home({
 }) {
   return (
     <>
+      <div>
+        <Image
+          src="/cabinCroped.jpg"
+          alt="cabin"
+          height={250}
+          width={500}
+          layout="responsive"
+        />
+      </div>
+      <div className="mt-2 p-2 rounded-md bg-slate-200 sm:bg-red-200 md:bg-green-200 lg:bg-blue-100 xl:bg-orange-200  flex justify-center items-center flex-col">
+        <h1 className="text-2xl font-semibold">Morfars Stuga</h1>
+        <h2 className="text-xl font-semibold">
+          Välkommen till vår fjällstuga i vackra Tänndalen
+        </h2>
+      </div>
       <PopUpQandA temp={items} />
       <Greeting />
       <Link href="/questionsAndAnswers">Questions and Answers</Link>

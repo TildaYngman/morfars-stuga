1e39cf46d04934cafc182ebaa941568d9c5b7cf7
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-09-01 16:23:12 +0200

    Navbar with hamburger menu is working but nicer styling is needed

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -4,17 +4,17 @@ export default function Navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <div className="flex items-center justify-between border-b border-gray-400 py-8">
-      <h1>LOGO</h1>
+    <div className="flex items-center px-6 justify-between bg-primary-green-100 border-b border-primary-green-200 py-5 ">
+      <h1 className="text-2xl font-semibold">logo</h1>
       <nav>
         <section className="flex lg:hidden">
           <div
             className="space-y-2"
             onClick={() => setIsNavOpen((prev) => !prev)}
           >
-            <span className="block h-0.5 w-8  bg-gray-600"></span>
-            <span className="block h-0.5 w-8  bg-gray-600"></span>
-            <span className="block h-0.5 w-8  bg-gray-600"></span>
+            <span className="block h-0.5 w-8  bg-primary-green-900"></span>
+            <span className="block h-0.5 w-8  bg-primary-green-900"></span>
+            <span className="block h-0.5 w-8  bg-primary-green-900"></span>
           </div>
 
           <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
@@ -36,22 +36,22 @@ export default function Navbar() {
               </svg>
             </div>
             <ul className="flex flex-col items-center justify-between min-h-[250px]">
-              <li className="border-b border-gray-400 my-8 uppercase">
+              <li className="border-b border-primary-green-300 my-6 uppercase">
                 <a href="/about">Om stugan</a>
               </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="/portfolio">Boka</a>
+              <li className="border-b border-primary-green-300 my-6 uppercase">
+                <a href="/bookingRequest">Bokningsförfrågan</a>
               </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
+              <li className="border-b border-primary-green-300 my-6 uppercase">
                 <a href="/contact">Kontakt</a>
               </li>
             </ul>
           </div>
         </section>
 
-        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
+        <ul className="hidden space-x-8 lg:flex">
           <li>
-            <a href="/about">About</a>
+            <a href="/about">Om stugan</a>
           </li>
           <li>
             <a href="/portfolio">Portfolio</a>

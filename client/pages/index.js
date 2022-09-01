7dea6679800da1b075945f476578445d697db0b3
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-09-01 14:27:10 +0200

    Removed import of greeting in the index file

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import { Greeting, MessageForm, PopUpQandA } from "../components/index";
+import {  MessageForm, PopUpQandA } from "../components/index";
 import { useState } from "react";
 
 export default function Home({

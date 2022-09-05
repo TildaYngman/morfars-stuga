8d48f083d12f2f3840b019fdd4ae383b7175b7a9
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-09-05 14:34:37 +0200

    Navbar with basic styling done

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,88 +2,76 @@ import { BsHouse } from "react-icons/bs";
 import { useState } from "react";
 
 export default function Navbar() {
-  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [navbar, setNavbar] = useState(false);
 
   return (
-    <nav className="flex justify-between bg-opacity-80 h-14 px-10">
-      <div className="px-3 pt-2">
-        <BsHouse size={40} color="#64748b" />
-      </div>
-      <div>
-        <section className="flex lg:hidden">
-          <div
-            className="space-y-2"
-            onClick={() => setIsNavOpen((prev) => !prev)}
-          >
-            <span className="block h-0.5 w-8  bg-primary-green-900"></span>
-            <span className="block h-0.5 w-8  bg-primary-green-900"></span>
-            <span className="block h-0.5 w-8  bg-primary-green-900"></span>
-          </div>
-
-          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
-            <div
-              className="absolute top-0 right-0 px-8 py-8"
-              onClick={() => setIsNavOpen(false)}
-            >
-              <svg
-                className="h-8 w-8 text-gray-600"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="18" y1="6" x2="6" y2="18" />
-                <line x1="6" y1="6" x2="18" y2="18" />
-              </svg>
+    <div>
+      <nav className="w-full">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+          <div>
+            <div className="flex items-center justify-between md:block">
+              <div className="px-3">
+                <BsHouse size={40} color="#64748b" />
+              </div>
+              <div className="md:hidden">
+                <button
+                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 text-slate-500"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 slate-500"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
             </div>
-            <ul className="flex flex-col items-center justify-between">
-              <li className=" pr-4 border-b border-primary-green-300 my-6 uppercase ">
-                <a href="/about">Om stugan</a>
-              </li>
-              <li className="border-b border-primary-green-300 my-6 uppercase">
-                <a href="/bookingRequest">Bokningsförfrågan</a>
-              </li>
-              <li className="border-b border-primary-green-300 my-6 uppercase">
-                <a href="/contact">Kontakt</a>
-              </li>
-            </ul>
           </div>
-        </section>
-        <div>
-          <ul className="lg:flex">
-            <li className="text-slate-500 border-x-2 border-white hover:border-x-2 hover:border-slate-400 hover:p-3 p-3 duration-150 text-xl font-semibold">
-              <a href="/about">Om stugan</a>
-            </li>
-            <li className="text-slate-500 border-x-2 border-white hover:border-x-2 hover:border-slate-400 hover:p-3 p-3 duration-150 text-xl font-semibold">
-              <a href="/bookingRequest">Bokningsförfrågan</a>
-            </li>
-            <li className="text-slate-500 border-x-2 border-white hover:border-x-2 hover:border-slate-400 hover:p-3 p-3 duration-150 text-xl font-semibold">
-              <a href="/">Kontakt</a>
-            </li>
-          </ul>
+          <div>
+            <div
+              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+                navbar ? "block" : "hidden"
+              }`}
+            >
+              <ul className="md:flex md:justify-end flex-row">
+                <li className="text-slate-500 border-x-2 border-white hover:border-x-2 hover:border-slate-400 hover:p-3 p-3 duration-150 text-xl font-semibold">
+                  <a href="/about">Om stugan</a>
+                </li>
+                <li className="text-slate-500 border-x-2 border-white hover:border-x-2 hover:border-slate-400 hover:p-3 p-3 duration-150 text-xl font-semibold">
+                  <a href="/about">Bokingsföfrågan</a>
+                </li>
+                <li className="text-slate-500 border-x-2 border-white hover:border-x-2 hover:border-slate-400 hover:p-3 p-3 duration-150 text-xl font-semibold">
+                  <a href="/about">Kontakt</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
-      <style>{`
-      .hideMenuNav {
-        display: none;
-      }
-      .showMenuNav {
-        display: block;
-        position: absolute;
-        width: 100%;
-        height: 100vh;
-        top: 0;
-        left: 0;
-        background: white;
-        z-index: 10;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-evenly;
-        align-items: center;
-      }
-    `}</style>
-    </nav>
+      </nav>
+    </div>
   );
 }

6de1d66444ff2b886e1baeaabca23d3c8bb311db
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-09-06 10:48:10 +0200

    Removed unecessary imports

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,4 @@
-import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import Link from "next/link";
-import { MessageForm, PopUpQandA } from "../components/index";
-import { useState } from "react";
 
 export default function Home({
   Component,

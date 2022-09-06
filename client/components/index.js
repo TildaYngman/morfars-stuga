32984d3611a1f4fbcd11f8c2b3c868e1407b9455
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-09-06 10:13:00 +0200

    Exporting Footer from components/index

--- client/components/index.js
+++ client/components/index.js
@@ -5,3 +5,4 @@ export { default as PopUpQandA } from "./PopUpQandA";
 export { default as QuestionCategory } from "./QuestionCategory";
 export { default as BookingInformation } from "./BookingInformation";
 export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";

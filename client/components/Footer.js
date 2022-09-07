967ee6b6fa0bb47f0bc9b4c05bd83bfb891674ee
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-09-07 11:18:07 +0200

    Changed paddingX

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -12,19 +12,18 @@ export default function Footer({
 }) {
   return (
     <>
-      <div className="flex bg-slate-100 border-2 border-slate-200 justify-center w-screen shadow-lg rounded-t-lg">
-        <div>
-          <MessageForm
-            setGuestName={setGuestName}
-            setEmail={setEmail}
-            setMessage={setMessage}
-            setTitle={setTitle}
-            guestName={guestName}
-            email={email}
-            message={message}
-            title={title}
-          />
-        </div>
+      <div className="flex bg-slate-100 justify-center w-full shadow-lg rounded-t-lg">
+        <MessageForm
+          setGuestName={setGuestName}
+          setEmail={setEmail}
+          setMessage={setMessage}
+          setTitle={setTitle}
+          guestName={guestName}
+          email={email}
+          message={message}
+          title={title}
+        />
+        <em></em>
       </div>
     </>
   );

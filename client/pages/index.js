1129892da3306a36950d6235301f056a4e12efe3
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-09-09 11:31:02 +0200

    initial commit

--- client/pages/index.js
+++ client/pages/index.js
@@ -11,7 +11,7 @@ export default function Home({ Component, pageProps }) {
           width={500}
           layout="responsive"
           className="absolute top-0 left-0"
-          priority
+          priority={true}
         />
       </div>
       <div className="mt-2 p-2 rounded-md flex justify-center items-center flex-col">

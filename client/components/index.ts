2f9ebc139c105e12d623ffcc7615236461163475
Tilda Yngman <Matilda.yngman@hyperisland.se>
2023-05-26 10:21:29 +0200

    Creating a new component for faqWidget

--- client/components/index.ts
+++ client/components/index.ts
@@ -3,3 +3,4 @@ export { default as MessageForm } from "./MessageForm";
 export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
 export { default as HeroImage } from "./HeroImage";
+export { default as FaqWidget } from "./FaqWidget";

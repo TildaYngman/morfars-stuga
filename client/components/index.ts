a1632e994913052c48b4278756093970a5a61302
Tilda Yngman <Matilda.yngman@hyperisland.se>
2023-05-26 10:18:33 +0200

    Removing all Q&A related files, components and code

--- client/components/index.ts
+++ client/components/index.ts
@@ -1,9 +1,5 @@
 export { default as Bookingform } from "./Bookingform";
 export { default as MessageForm } from "./MessageForm";
-export { default as QuestionsAndAnswersComponent } from "./questionsAndAnswersComponent";
-export { default as PopUpQandA } from "./PopUpQandA";
-export { default as QuestionCategory } from "./QuestionCategory";
-export { default as BookingInformation } from "./BookingInformation";
 export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
 export { default as HeroImage } from "./HeroImage";

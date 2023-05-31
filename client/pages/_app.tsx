b0bb066f0115ba519d3784e0b35d3baf6d09bf8e
Tilda Yngman <Matilda.yngman@hyperisland.se>
2023-05-31 15:46:13 +0200

    Adding a favicon and changing text styling

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,10 +2,14 @@ import "../styles/globals.css";
 import { Navbar, Footer } from "../components/index";
 import React from "react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />

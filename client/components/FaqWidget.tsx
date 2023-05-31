c1b3a6f0f73f124e50e5a18b50b4b058d22ccd63
Tilda Yngman <Matilda.yngman@hyperisland.se>
2023-05-31 10:43:11 +0200

    WIP- faq imbeded is working

--- client/components/FaqWidget.tsx
+++ client/components/FaqWidget.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import { useEffect } from "react";
+import * as React from "react";
 
-export default function FaqWidget() {
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      "webcomet-faq": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement>,
+        HTMLElement
+      >;
+    }
+  }
+}
+
+const FaqWidget: React.FC = () => {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.type = "module";
+    script.src =
+      "https://app.webcomet.io/webcomet-components/webcomet-components.esm.js";
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
-    <>
-
-    </>
+    <div>
+      <webcomet-faq faq-id="869-morfarsstuga"></webcomet-faq>
+    </div>
   );
-}
+};
+
+export default FaqWidget;

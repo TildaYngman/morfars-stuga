8dff02699d1a87a5bbc0fc124f65054c1d7e8400
Tilda Yngman <Matilda.yngman@hyperisland.se>
2023-06-01 13:47:52 +0200

    Adding a new about text and changing responsive styling on gallery

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -9,17 +9,42 @@ export default function about() {
           Om Morfars Stuga
         </h1>
         <p className="pt-2 text-slate-800">
-          Här njuter vi av underbara dagar i den allra vackraste miljön som
-          Härjedalen har att erbjuda. Vi önskar er krispiga dagar på fjället och
-          varma kvällar i bastun.Här njuter vi av underbara dagar i den allra
-          vackraste miljön som Härjedalen har att erbjuda. Vi önskar er krispiga
-          dagar på fjället och varma kvällar i bastun.Här njuter vi av underbara
-          dagar i den allra vackraste miljön som Härjedalen har att erbjuda. Vi
-          önskar er krispiga dagar på fjället och varma kvällar i bastun.Här
-          njuter vi av underbara dagar i den allra vackraste miljön som
-          Härjedalen har att erbjuda. Vi önskar er krispiga dagar på fjället och
-          varma kvällar i bastun.Här njuter vi av underbara dagar i den allra
-          vackraste miljön som Härjedalen har att erbjuda.
+          Välkommen till vår vackra fjällstuga i Tänndalen. Den större av de två
+          stugorna är tillgänglig att hyra. Storstugan byggdes på 80-talet och
+          har gått i arv från vår mormor och morfar till vår mamma och nu har vi
+          äran att förvalta den. Stugan har skänkt glädje och fantastiska minnen
+          till vår familj. Vi vill vårda detta arv och sprida samma glädje till
+          kommande generationer. Med omsorgsfull planering har vår familj skapat
+          en stuga utrustad med allt för en fantastisk fjällvistelse. Morfar var
+          en uppfinnare och en entreprenör och har skapat många av de finurliga
+          lösningar som finns i stugans inredning. Välkommen till en unik plats
+          där du kan känna dig hemma och uppleva fjällens magi. Låt oss vara din
+          värd för en oförglömlig fjällvistelse.
+          <br /> <br /> På entreplan, det översta av två våningsplan, möts du av
+          ett fullt utrustat kök där du kan laga läckra måltider och njuta av
+          den vackra fjällmiljön. Ett trevligt vardagsrum välkomnar dig med sin
+          mysiga braskamin, perfekt för att koppla av och varma upp sig efter en dag
+          utomhus. På detta våningsplan finner du även en av stugans två
+          toaletter.
+          <br /> <br /> Stugan erbjuder också ett bekvämt dubbelrum med en säng
+          på 160 cm, komplett med praktisk förvaring under sängen. För familjer
+          eller extra gäster finns ett mindre sovrum med våningssäng och
+          garderob. Dessutom erbjuder stugan flera hallar med gott om
+          förvaringsmöjligheter för ytterkläder och en speciell plats för att
+          torka pjäxorna. <br /> <br /> En trappa ner väntar ett extra allrum
+          där du kan koppla av och spendera tid tillsammans. Här hittar du även
+          en frys som är lättillgänglig under trappan. På detta
+          våningsplan finns även en mindre toalett som du kan nå från allrummet.
+          Ett mysigt sovrum med våningssäng, garderob och byrå erbjuder
+          ytterligare utrymme för avkoppling och en god natts sömn. Dessutom
+          finns här ingången till duschrummet och bastun, där du kan unna dig
+          välbehövlig avkoppling efter en aktiv dag i backen eller längdspåren.
+          <br /> <br /> Vår fjällstuga är mer än bara en plats att bo på - den
+          är en oas av lugn och skönhet mitt i natursköna Tänndalen. Vi har
+          lagt stor omsorg i att göra den till en plats där du kan skapa minnen
+          och njuta av en fantastisk fjällupplevelse. Vi ser fram emot att
+          välkomna dig till vår stuga och ge dig en oförglömlig vistelse i
+          fjällen.
         </p>
       </div>
       <ImageGallery />
